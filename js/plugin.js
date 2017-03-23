40532d43ef66c5b38d490d0469b6fd4197be5b66
jilliancain <jillian.d.cain@gmail.com>
2017-03-22 21:18:39 -0700

    Adding plug ins

--- js/plugin.js
+++ js/plugin.js
@@ -1,3 +1,13 @@
 // Paste jQuery Plugin Code Here
 
+navigator.geolocation.getCurrentPosition(function(position){
+	var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+	var latAndLong = latitude + ',' + longitude; 
+  console.log(latAndLong);
+	return latAndLong;
+});
+
+
+
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
 // YOUR SCRIPTS GO HERE
+
 $.simpleWeather({
-  location: 99004,
+    location: 98004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
+      
+
+      // Change Change w/ Condition code
+      var iconCode = 'icon-' + weather.code;
+      $('i').attr('class', iconCode );
+      
+      // Condition Code
+      // Reference Condition Codes
+      // https://developer.yahoo.com/weather/documentation.html#codes
+      console.log(weather.code);
+      
+      // If Sunny `code` between 31 and 36       
+      if (weather.code >= 31 && weather.code <= 36 ) {
+        
+        $('body').addClass('sunny');
+        
+      }
+      
+       // If Cloudy `code` between 31 and 36       
+      if (weather.code >= 26 && weather.code <= 30 ) {
+        
+        $('body').addClass('cloudy');
+        
+      }
+      
+      // If Rainy `code` between 31 and 36       
+      if (weather.code >= 5 && weather.code <= 14 ) {
+        
+        $('body').addClass('rainy');
+        
+      }
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
   
-  unit: 'f',
-  
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city);
-    $('.state').text(weather.region);
-    $('.image img').attr('src', weather.image);
-    
-  },
-  
-  error: function(error) {
-    console.log('Look outside.');
-  }
-});
+  });
+
+
+//sidr
+
+$('#sidr-show-hide').sidr();
+
+// Close from inside
+$('#close').sidr({
+  method: 'close'
+})
+
+
+
